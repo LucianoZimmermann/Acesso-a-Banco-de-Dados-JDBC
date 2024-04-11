084636feefcbdee5bb1731644f4a3fef7ae31e81
Luciano Zimmermann <lucianolz123@gmail.com>
2024-04-11 14:33:17 -0300

    Sending Files

---
+++ demo-dao-jdbc/src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.Date;
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entites.Department;
+import model.entites.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("Test Seller findbyId");
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+		
+		System.out.println("\nTest Seller findByDepartment");
+		
+		Department department = new Department(2, null);
+		
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\nTest Seller findAll");
+		
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\nTest Seller insert");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted! New ID = " + newSeller.getId());
+
+	}
+
+}
